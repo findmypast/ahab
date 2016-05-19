f13565888c3a1027df544337134b20e12b61e232
Jae Bach Hardie <jhardie@findmypast.com>
2016-05-19 12:43:46 +0100

    Communicating with consul API

--- src/search.js
+++ src/search.js
@@ -1,5 +1,29 @@
 'use strict';
 
+function getContainerNames(result) {
+  return result;
+}
+
 module.exports = (args) => {
-  console.log(args);
+  const consulHostParts = args.consul.split(':');
+  const consulHost = consulHostParts[0];
+  const consulPort = consulHostParts[1];
+
+  const consul = require('consul')({
+    host: consulHost,
+    port: consulPort,
+    promisify: true
+  });
+
+  return consul.catalog.service.nodes(
+    {
+      service: args.service,
+      tag: args.tag
+    })
+  .then((res) => {
+    console.log(getContainerNames(res));
+  })
+  .catch((err) => {
+    throw err;
+  });
 };
