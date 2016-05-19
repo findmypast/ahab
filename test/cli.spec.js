1fb7d155d951c4c90958eedaf9eb2b57d3aca427
Mike Thomas <mthomas@findmypast.com>
2016-05-19 16:10:36 +0100

    Add test for multiple container names
    Pair: Jae

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,18 +1,20 @@
-/* global describe it before */
+/* global describe it before afterEach */
 /* eslint no-unused-expressions: "off" */
 'use strict';
 
 const expect = require('chai').expect;
 const exec = require('child_process').exec;
 
-const server = require('express')();
+const express = require('express');
 const mockPort = 33333;
+let expressServer = null;
 
 function mockServer(response, done) {
+  const server = express();
   server.get('/*', (req, res) => {
     res.send(response);
   });
-  server.listen(mockPort, () => {
+  expressServer = server.listen(mockPort, () => {
     done();
   });
 }
@@ -24,7 +26,11 @@ function act(service, tag, dockerHost, done) {
   );
 }
 
-describe('when querying for a service with one container on one tag', function() {
+afterEach(function(done) {
+  expressServer.close(done);
+});
+
+describe('when querying for a service with one container on one tag on one host', function() {
   const testHost = 'test-host';
   const testContainer = 'happy-test-container';
   before(function(done) {
@@ -36,13 +42,41 @@ describe('when querying for a service with one container on one tag', function()
     );
   });
 
-  it('should return container name for a service running one container', function(done) {
+  it('should return the container name', function(done) {
     act(
       'happy-test',
       'test',
-      'test-host',
+      testHost,
       (err, stdout) => {
-        expect(stdout).to.equal('happy-test-container');
+        expect(stdout).to.equal(`${testContainer}\n`);
+        done(err);
+      }
+    );
+  });
+});
+
+describe('when querying for a service with multiple containers on one tag', function() {
+  const testHost = 'test-host';
+  const testContainers = ['happy-test-container', 'another-test-container'];
+  before(function(done) {
+    mockServer(
+      [{
+        'ServiceID': `${testHost}:${testContainers[0]}:4001`
+      },
+      {
+        'ServiceID': `${testHost}:${testContainers[1]}:4001`
+      }],
+      done
+    );
+  });
+
+  it('should return the container names', function(done) {
+    act(
+      'happy-test',
+      'test',
+      testHost,
+      (err, stdout) => {
+        expect(stdout).to.equal(testContainers.join('\n') + '\n');
         done(err);
       }
     );
